bbdbe4d45019360653110b113bb6706872ebcfa3
nodarchik <chikadzee@gmail.com>
2023-02-06 12:35:29 +0400

    Photo localstorage works

--- src/second_page/script.js
+++ src/second_page/script.js
@@ -7,12 +7,19 @@ const aboutInput = document.getElementById("about__id");
 const emailInput = document.getElementById("email__id");
 const phoneInput = document.getElementById("phone__id");
 const fileButton = document.getElementById("fileBtn");
+const arrowImg = document.getElementById("arrow__img");
+const outputName = document.getElementById("output_name");
+const outputAbout = document.getElementById("output_about");
+const outputPhoto = document.getElementById("output_photo");
+const outputEmail = document.getElementById("output_email");
+const outputPhone = document.getElementById("output_phone");
+
 // Constants for Regex
 const georgian = /^[ა-ჰ\s]+$/;
 const redberry = /^[a-zA-Z0-9._%+-]+@redberry.ge$/;
-const phonePattern =  /^\+995\s*\d{3}\s*\d{2}\s*\d{2}\s*\d{2}$/;
+const phonePattern = /^\+995\s*\d{3}\s*\d{2}\s*\d{2}\s*\d{2}$/;
 
-// Form validation localstorage get inputs.
+// Localstorage get inputs.
 if (localStorage.getItem("fname")) {
   fnameInput.value = localStorage.getItem("fname");
 }
@@ -20,7 +27,7 @@ if (localStorage.getItem("lname")) {
   lnameInput.value = localStorage.getItem("lname");
 }
 // if (localStorage.getItem("photo")) {
-//   photoInput.value = localStorage.getItem("photo");
+//   photoInput.value = localStorage.getItem('photo');
 // }
 if (localStorage.getItem("about")) {
   aboutInput.value = localStorage.getItem("about");
@@ -32,80 +39,86 @@ if (localStorage.getItem("phone")) {
   phoneInput.value = localStorage.getItem("phone");
 }
 
-
-// photoInput.addEventListener("change", function () {
-//   const file = photoInput.files[0];
-
-//   // Check if file is an image
-//   if (!file.type.startsWith("image/")) {
-//     return;
-//   }
-
-//   const reader = new FileReader();
-//   reader.readAsDataURL(file);
-//   reader.onload = function () {
-//     // Save the photo data as a base64 string to local storage
-//     localStorage.setItem("photo", reader.result);
-//   };
-// });
-// const photoData = localStorage.getItem("photo");
-
-// if (photoData) {
-//   const photoImg = document.createElement("img");
-//   photoImg.src = photoData;
-//   document.body.appendChild(photoImg);
-// }
-
-
-
-
-
-
 // Event listener for form.
-form.addEventListener("submit", function(event) {
-
-// Prevent page refresh on submit button.
+form.addEventListener("keyup", function (event) {
+  // Prevent page refresh on submit button.
   event.preventDefault();
 
-// Constants to save input value.
+  // Constants to save input value.
   const fname = fnameInput.value;
   const lname = lnameInput.value;
-  // const photo = photoInput.value;
   const about = aboutInput.value;
   const email = emailInput.value;
   const phone = phoneInput.value;
 
-// Save input data to localstorage.
+  // Save input data to localstorage.
   localStorage.setItem("fname", fname);
   localStorage.setItem("lname", lname);
-  // localStorage.setItem("photo", photo);
   localStorage.setItem("about", about);
   localStorage.setItem("email", email);
   localStorage.setItem("phone", phone);
 
-// Form validation logic.
-  if (fnameInput.value.length < 2 || !georgian.test(fnameInput.value)) {
-    alert("გთხოვთ შეიყვანოთ სახელი მინიმუმ 2 ქართული სიმბოლოს გამოყენებით");
-  }
-  if (lnameInput.value.length < 2 || !georgian.test(lnameInput.value)){alert("გთხოვთ შეიყვანოთ გვარი მინიმუმ 2 ქართული სიმბოლოს გამოყენებით");
-  }
-  // if (!photoInput.value) {
-  //   alert("სურათის ატვირთვა სავალდებულოა");
-  // }
-  if (!redberry.test(emailInput.value)) {
-    alert("ემეილი უნდა მთავრდებოდეს @redberry.ge-თი");
-  } 
-  if (!phonePattern.test(phoneInput.value)) {
-    alert("ტელეფონის ნომერი უნდა შეესაბამებოდეს ქართული ნომრის ფორმატს");
-  }
-  else {
-    document.getElementById("nextBtn").onclick = function () {
-      location.href = "third.html";
-  };
-  }
+
+
+// Listen for changes on the input element
+photoInput.addEventListener('change', async (event) => {
+  // Get the selected file
+  const file = event.target.files[0];
+
+  // Convert the file to a Base64 encoded string
+  const base64 = await toBase64(file);
+
+  // Save the Base64 encoded string in local storage
+  localStorage.setItem('photo', base64);
 });
 
+// Utility function to convert a file to a Base64 encoded string
+async function toBase64(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result.split(',')[1]);
+    reader.onerror = error => reject(error);
+  });
+}
+
+
+
+  // Display input simultenously.
+  outputName.innerHTML = fname + " " + lname;
+  outputAbout.innerHTML = about;
+  outputEmail.innerHTML = email;
+  outputPhone.innerHTML = phone;
+  outputPhoto.setAttribute("src", "data:image/jpeg;base64," + localStorage.getItem("photo"));
+
+  // // Form validation logic.
+  // if (fnameInput.value.length < 2 || !georgian.test(fnameInput.value)) {
+  //   alert("გთხოვთ შეიყვანოთ სახელი მინიმუმ 2 ქართული სიმბოლოს გამოყენებით");
+  // }
+  // if (lnameInput.value.length < 2 || !georgian.test(lnameInput.value)) {
+  //   alert("გთხოვთ შეიყვანოთ გვარი მინიმუმ 2 ქართული სიმბოლოს გამოყენებით");
+  // }
+  // // if (!photoInput.value) {
+  // //   alert("სურათის ატვირთვა სავალდებულოა");
+  // // }
+  // if (!redberry.test(emailInput.value)) {
+  //   alert("ემეილი უნდა მთავრდებოდეს @redberry.ge-თი");
+  // }
+  // if (!phonePattern.test(phoneInput.value)) {
+  //   alert("ტელეფონის ნომერი უნდა შეესაბამებოდეს ქართული ნომრის ფორმატს");
+  // } else {
+  //   document.getElementById("nextBtn").onclick = function () {
+  //     location.href = "third.html";
+  //   };
+  // }
+});
+
+arrowImg.onclick = function () {
+  localStorage.clear();
+  location.href = "../../index.html";
+};
+
 // Button styling
-fileButton.addEventListener("click", function() {
+fileButton.addEventListener("click", function () {
   photoInput.click();
 });
